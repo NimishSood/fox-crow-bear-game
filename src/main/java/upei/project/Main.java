b918b705f4f91dd278da63752b2546dc91ec3b03
Adi <adityasagarpython@gmail.com>
2024-11-26 11:01:37 -0400

    Made print generic and code refactor

--- src/main/java/upei/project/Main.java
+++ src/main/java/upei/project/Main.java
@@ -4,18 +4,6 @@ package upei.project;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    private int exampleVar = 0;
-
-    public void setExampleVar(int inVal)
-    {
-        exampleVar = 0;
-    }
-
-    public int getExampleVar()
-    {
-        return exampleVar;
-    }
-
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
